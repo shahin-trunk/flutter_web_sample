aa6988ffe147c15c36b3d06cae652b2824b79462
Shahin Konadath <58107095+shahin-trunk@users.noreply.github.com>
2022-03-25 08:52:39 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_site/flutter_firebase_site.dart';
+
+void main() {
+  runApp(FlutterFirebaseSite());
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.yellow,
+  primaryColor: Colors.yellow,
+  backgroundColor: Color(0xFF151515),
+  scaffoldBackgroundColor: Color(0xFF151515),
+  canvasColor: Colors.yellow,
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 92,
+      color: Colors.white,
+      fontWeight: FontWeight.w900,
+    ),
+    headline3: TextStyle(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w300,
+    ),
+  ),
+);
